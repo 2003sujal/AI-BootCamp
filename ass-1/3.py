d4c5c963d0818d4dec2822850ea4436e7a62ab42
Sujal Luitel <sujalluitel2003@gmail.com>
2026-07-16 18:53:31 +0545

    updated

---
+++ ass-1/3.py
@@ -0,0 +1,7 @@
+#to check odd or even number 
+num = int(input("Enter a number: "))
+
+if num % 2 == 0:
+    print(num, "is Even")
+else:
+    print(num, "is Odd")
---
+++ ass-1/5.py
@@ -0,0 +1,11 @@
+#to print factorial of a number 
+num = int(input("Enter a number: "))
+
+factorial = 1
+
+if num < 0:
+    print("Factorial does not exist for negative numbers.")
+else:
+    for i in range(1, num + 1):
+        factorial *= i
+    print("Factorial =", factorial)
